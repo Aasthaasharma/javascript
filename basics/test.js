d36bfcef60ab088ad3bb7ef306fa3a02740fd66d
Aastha Sharma <132471408+Aasthaasharma@users.noreply.github.com>
2024-01-07 16:10:12 +0000

    datatypes

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,15 @@
 console.log("hello")
+const accountId=1235
+let accountEmail="dfhfe"
+var pass="54" //don't use
+city="lucknow"
+
+// accountId=456
+// console.log(accountId); not allowed
+accountEmail="atdwt"
+console.log(accountEmail);
+pass="5988"
+console.log(pass);
+city="luckw"
+console.log(city);
+console.table([accountId,accountEmail,pass,city])
